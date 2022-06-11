cfc12d68ceff125f270d4d3f5aaba0f760bf6d40
bans04 <bansodednyanadeo@gmail.com>
2022-06-11 23:22:13 +0530

    Added:-> Calculate daily employee wage as per full time or part time

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -3,6 +3,9 @@ package com.employeewage;
  * --------------------Employee Wage Computation Program--------------------
  *Print Welcome message
  *-> Check Employee is Present or Absent
+ *-> Calculate Employee daily wage
+ *
+ *@Dnyandeo Banosde
  *
  **/
 
@@ -10,16 +13,20 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program..|");
-		int employeePresent = 1;
+		//int employeePresent = 1;
+		int fullTime = 1;
+		int partTime = 2;
 		int employeeWagePerHr = 20;
 		int employeeHr = 0;
-		int random = (int) (Math.floor(Math.random() * 10) % 2);
+		int random = (int) (Math.floor(Math.random() * 10) % 3);
 		
-		if(random == employeePresent) {
-			System.out.println("Employee is Present..|");
+		if(random == fullTime) {
+			System.out.println("Employee is Present full time..|");
 			employeeHr = 8;
 		}else {
-			System.out.println("Employee is Absent..|");
+			//System.out.println("Employee is Absent..|");
+			System.out.println("Emplyee is present part time..|");
+			employeeHr = 4;
 		}
 		
 		System.out.println("Daily employee wage is :-> " + employeeWagePerHr * employeeHr);
