336586bfa9ffcaef9eff07be98f1c1f4877a46a4
bans04 <bansodednyanadeo@gmail.com>
2022-06-11 22:06:05 +0530

    Added:-> Print Welcome message on Master Branch

---
+++ src/com/employeewage/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.employeewage;
+/*
+ * ---------------------Employee Wage Computation Program---------------------
+ *Print Welcome message
+ *
+ **/
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program..|");
+	}
+}
