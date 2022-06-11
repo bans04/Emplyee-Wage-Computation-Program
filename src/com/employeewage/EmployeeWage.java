67795495af926393a7a50685a4db37a297c02d5d
bans04 <bansodednyanadeo@gmail.com>
2022-06-11 23:10:30 +0530

    Added:-> Calculate employee daily wage where hr = 8 and wage/Hr = 20

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -11,12 +11,17 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program..|");
 		int employeePresent = 1;
+		int employeeWagePerHr = 20;
+		int employeeHr = 0;
 		int random = (int) (Math.floor(Math.random() * 10) % 2);
 		
 		if(random == employeePresent) {
 			System.out.println("Employee is Present..|");
+			employeeHr = 8;
 		}else {
 			System.out.println("Employee is Absent..|");
 		}
+		
+		System.out.println("Daily employee wage is :-> " + employeeWagePerHr * employeeHr);
 	}
 }
